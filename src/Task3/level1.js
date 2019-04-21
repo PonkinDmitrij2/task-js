533919c7ce521aa4eab079d5477bd0516fd674d0
Nikita Kamyshenko <nikita.kamyshenko@mail.ru>
2019-04-21 20:25:12 +0300

    Added task 3

---
+++ src/Task3/level1.js
@@ -0,0 +1,32 @@
+/**
+ * На вход принимаем блюда которые нужно приготовить.
+ * Готовить нужно по книге рецептов.
+ * Следовательно для приголовки omelet в консоль нужно вывести Cook omelet.
+ *
+ * dish - блюдо
+ * dishes - блюда
+ * recipe - рецепт
+ * recipeBook - книга рецептов
+ */
+
+
+const recipeBook = {
+    omelet: () => console.log('Cook omelet.'),
+    cake: () => console.log('Cook сake.'),
+    steak: () => console.log('Cook steak.'),
+};
+
+const cookDishes = (dishes) => {
+    //TODO: implement this
+};
+
+const dishes = ['omelet', 'cake', 'steak'];
+
+cookDishes(dishes);
+
+/**
+ * Вывод:
+ * Cook omelet.
+ * Cook сake.
+ * Cook steak.
+ */
